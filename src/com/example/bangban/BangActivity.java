516ae8355a0d62fb8aafab30746efd0db9374f6c
jay bay <gratholio@gmail.com>
2014-03-18 20:28:18 -0400

    CH2 Challanges

--- src/com/example/bangban/BangActivity.java
+++ src/com/example/bangban/BangActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,7 +13,8 @@ public class BangActivity extends Activity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private Button mNextButton;
+	private ImageButton mPrevButton;
+	private ImageButton mNextButton;
 	private TextView mQuestionTextView;
 
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -24,42 +26,48 @@ public class BangActivity extends Activity {
 
 	private int mCurrentIndex = 0;
 
-	
 	private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getQuestion();
-        mQuestionTextView.setText(question);
-    }
-	
-	 private void checkAnswer(boolean userPressedTrue) {
-	        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
 
-	        int messageResId = 0;
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 
-	        if (userPressedTrue == answerIsTrue) {
-	            messageResId = R.string.correct_toast;
-	        } else {
-	            messageResId = R.string.incorrect_toast;
-	        }
+		int messageResId = 0;
 
-	        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-	            .show();
-	    }
-	
+		if (userPressedTrue == answerIsTrue) {
+			messageResId = R.string.correct_toast;
+		} else {
+			messageResId = R.string.incorrect_toast;
+		}
+
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bang);
 
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
 
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				checkAnswer(true);
 			}
 		});
+		
 		mFalseButton = (Button) findViewById(R.id.false_button);
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -68,7 +76,19 @@ public class BangActivity extends Activity {
 			}
 		});
 
-		mNextButton = (Button) findViewById(R.id.next_button);
+		mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+		mPrevButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mCurrentIndex == 0)
+					mCurrentIndex = mQuestionBank.length;
+				mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		updateQuestion();
+
+		mNextButton = (ImageButton) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -77,7 +97,7 @@ public class BangActivity extends Activity {
 			}
 		});
 
-		updateQuestion();
+		// updateQuestion();
 	}
 
 	@Override
