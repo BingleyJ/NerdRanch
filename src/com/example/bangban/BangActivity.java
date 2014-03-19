e56cf7cf6dacc05e882a0d680c2486452a8f7085
jay bay <gratholio@gmail.com>
2014-03-18 21:39:54 -0400

    Added Score and made buttons big.

--- src/com/example/bangban/BangActivity.java
+++ src/com/example/bangban/BangActivity.java
@@ -2,6 +2,7 @@ package com.example.bangban;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -15,22 +16,29 @@ public class BangActivity extends Activity {
 	private Button mFalseButton;
 	private ImageButton mPrevButton;
 	private ImageButton mNextButton;
+	private TextView mQuestionScoreView;
 	private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
 
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
-			new TrueFalse(R.string.question_leonardfight, true),
+			new TrueFalse(R.string.question_leonardfight, false),
 			new TrueFalse(R.string.question_pennyact, false),
 			new TrueFalse(R.string.question_rajdrink, true),
 			new TrueFalse(R.string.question_sheldonspace, false),
 			new TrueFalse(R.string.question_sheldonsmart, true), };
 
 	private int mCurrentIndex = 0;
+	public int mScore = 0;
 
 	private void updateQuestion() {
 		int question = mQuestionBank[mCurrentIndex].getQuestion();
 		mQuestionTextView.setText(question);
 	}
-
+	
+	private void updateScore() {
+		mQuestionScoreView.setText(Integer.toString(mScore));
+	}
+	
 	private void checkAnswer(boolean userPressedTrue) {
 		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 
@@ -38,6 +46,8 @@ public class BangActivity extends Activity {
 
 		if (userPressedTrue == answerIsTrue) {
 			messageResId = R.string.correct_toast;
+			mScore = mScore + 100;
+			updateScore();
 		} else {
 			messageResId = R.string.incorrect_toast;
 		}
@@ -48,8 +58,12 @@ public class BangActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_bang);
 
+		mQuestionScoreView = (TextView) findViewById(R.id.mscore);
+		mQuestionScoreView.setText(Integer.toString(mScore));
+		
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -61,7 +75,6 @@ public class BangActivity extends Activity {
 
 		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
 				checkAnswer(true);
@@ -86,7 +99,6 @@ public class BangActivity extends Activity {
 				updateQuestion();
 			}
 		});
-		updateQuestion();
 
 		mNextButton = (ImageButton) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
@@ -97,8 +109,40 @@ public class BangActivity extends Activity {
 			}
 		});
 
-		// updateQuestion();
+		updateQuestion();
+		updateScore();
 	}
+	
+	@Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
